21671c43f6d4abfec35fd5631ff411cd64fb7cf1
MyUsername <my@email.com>
2023-03-02 17:36:29 +0100

    creation appmain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+   name: 'AppMain',
+   data() {
+      return {
+         post: [],
+         loading: false,
+         urlAddress: '',
+      }
+   },
+
+   methods: {
+      
+   },
+
+
+   created() {
+      
+   },
+}
+</script>
+
+
+
+<template >
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <h1>
+                  Hello
+               </h1>
+            </div>
+         </div>
+      </div>
+   </section>
+</template>
+
+<style lang="scss" scoped>
+   
+</style>
