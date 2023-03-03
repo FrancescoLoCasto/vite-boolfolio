868463d60438e318eb8a7f4c5a6e224229203177
MyUsername <my@email.com>
2023-03-03 15:41:59 +0100

    print card on dom

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -20,7 +20,7 @@ export default {
             }
          })
          .then((response) => {
-            this.posts = response.data.results.data;
+            this.posts = response.data.results;
          })
          .catch(function (error) {
             console.warn(error);
@@ -39,16 +39,23 @@ export default {
 
 <template >
    <section>
-      <div class="container">
-         <div class="row">
-            <div class="col-12">
-               <h1>
-                  Post:
-               </h1>
-
-               
-               <article class="col-6 single-post" v-for="post in posts" >
-                  {{ post.title }}
+      <div class="row">
+         <div class="col-12">
+            <h1>
+               Post:
+            </h1>
+            
+            
+            <div class="container d-flex flex-wrap">
+               <article class="col-6 single-post   card" v-for="post in posts" >
+                        <img :src="post.image" alt="">
+                     <div class="card-body">
+                        <h5 class="card-title">{{post.title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">{{ post.content }}</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                     </div>
                </article>
             </div>
          </div>
