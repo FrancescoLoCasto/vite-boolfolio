09b164688173ce47236406fce8602b62247a2b34
MyUsername <my@email.com>
2023-03-04 00:08:12 +0100

    appheadr add router link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,20 +10,29 @@ export default {
 
 
 <template lang="">
+
    <header>
-      <nav>
-         <ul class="d-flex">
-            <li class="d-inline m-3">
-               Home
-            </li>
-            <li class="d-inline m-3">
-               Posts
-            </li>
-            <li class="d-inline m-3">
-               Technology
-            </li>
-         </ul>
-      </nav>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+         <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+               <div class="navbar-nav">
+                  <router-link class="nav-link active" aria-current="page" :to="{name: 'home'}"> 
+                     Home
+                  </router-link> 
+                  <router-link class="nav-link active" aria-current="page" :to="{name: 'posts'}"> 
+                     Posts
+                  </router-link> 
+                  <router-link class="nav-link active" aria-current="page" :to="{name: 'about-us'}"> 
+                     AboutUs
+                  </router-link> 
+               </div>
+            </div>
+         </div>
+   </nav>
    </header>
 </template>
 
